12ac021d7abe2633f5ccdbf49e501a0bb5992d09
dahiajw <3460213800@qq.com>
2025-04-13 19:21:23 +0800

    first

---
+++ build/CMakeFiles/Tetris.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/study/大一下/c++/Lab/Lab2/Classes.cpp" "CMakeFiles/Tetris.dir/Classes.cpp.obj" "gcc" "CMakeFiles/Tetris.dir/Classes.cpp.obj.d"
+  "D:/study/大一下/c++/Lab/Lab2/Main.cpp" "CMakeFiles/Tetris.dir/Main.cpp.obj" "gcc" "CMakeFiles/Tetris.dir/Main.cpp.obj.d"
+  "D:/study/大一下/c++/Lab/Lab2/Performance.cpp" "CMakeFiles/Tetris.dir/Performance.cpp.obj" "gcc" "CMakeFiles/Tetris.dir/Performance.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
